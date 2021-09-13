a4de9a89ad775e397cd9d71d5c0de27da56eada8
Ntokozo <Ntokozoflex99@gmail.com>
2021-09-13 15:18:33 +0200

    Ntokozo

--- app/src/main/java/com/example/projectdoc/MainActivity.java
+++ app/src/main/java/com/example/projectdoc/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.projectdoc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button SignUpbutton;
+    Button LogInbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SignUpbutton= (Button)findViewById(R.id.signUp_btn);
+        LogInbutton= (Button)findViewById(R.id.loginBtn);
+
+        SignUpbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,SignUp.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        LogInbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,SignIn.class);
+                startActivity(intent);
+                Toast.makeText(MainActivity.this,"LOGGED IN!!",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
 }
